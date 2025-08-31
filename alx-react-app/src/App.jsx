ced1725f7aeaf67563cea7bb2193d76d8330f7fa
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-08-31 00:27:14 +0000

    Adding more different components to the page

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
-      {/* ...other components or content... */}
+      <Header />
+      <MainContent />
+      <Footer />
+      {/* ...include the tags of all relevant components or content... */}
     </div>
   );
 }
