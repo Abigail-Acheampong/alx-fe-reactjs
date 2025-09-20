c8caf90d2db1304b1d93fff40e5fc620a4cec7d4
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-09-20 11:24:39 +0000

    Corrected the codes

--- recipe-sharing-app/src/components/recipeDetails.jsx
+++ recipe-sharing-app/src/components/recipeDetails.jsx
@@ -1,24 +1,19 @@
-import { useRecipeStore } from './recipeStore'
-import EditRecipeForm from './EditRecipeForm'
-import DeleteRecipeButton from './DeleteRecipeButton'
+// RecipeDetails component
+  import { useRecipeStore } from './recipeStore';
 
-const RecipeDetails = ({ recipeId }) => {
-  const recipe = useRecipeStore(state =>
-    state.recipes.find(recipe => recipe.id === recipeId)
-  )
+  const RecipeDetails = ({ recipeId }) => {
+    const recipe = useRecipeStore(state =>
+      state.recipes.find(recipe => recipe.id === recipeId)
+    );
 
-  if (!recipe) {
-    return <div>Recipe not found</div>
-  }
-
-  return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.description}</p>
-      <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton recipeId={recipeId} />
-    </div>
-  )
-}
+    return (
+      <div>
+        <h1>{recipe.title}</h1>
+        <p>{recipe.description}</p>
+        <EditRecipeForm recipe={recipe} />
+        <DeleteRecipeButton recipeId={recipeId} />
+      </div>
+    );
+  };
 
 export default RecipeDetails
