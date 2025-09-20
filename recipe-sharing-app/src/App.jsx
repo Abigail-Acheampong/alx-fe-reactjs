b6b073fca1349a4ea1a438cfb0de33bec86c8d2c
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-09-20 08:58:59 +0000

    Extended the recipe store to have delete and update functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <RecipeList />
-      <AddRecipeForm />
+      <h1>Recipe Sharing Application</h1>
     </div>
   );
 }
